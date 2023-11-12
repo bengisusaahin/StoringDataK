c4b9f29b88efe6509f66fdf41a5a2349174d0b7c
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-12 16:47:02 +0300

    Read the data

--- app/src/main/java/com/bengisusahin/storingdata/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/storingdata/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bengisusahin.storingdata
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,15 +9,21 @@ import com.bengisusahin.storingdata.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //SharedPreferences - XML - Key-Value kucuk veritabanlari
+        sharedPref = getSharedPreferences("com.bengisusahin.storingdata", MODE_PRIVATE) //MODE_PRIVATE benim verielerime benim uygulamam ulassin sadece
     }
 
     fun save(view : View){
-
+        val myAge = binding.editText.text.toString()
+        binding.textView.text = "Your age: ${myAge}"
+        //binding.textView.text = "Your age: " + myAge
     }
 
     fun delete(view: View){
