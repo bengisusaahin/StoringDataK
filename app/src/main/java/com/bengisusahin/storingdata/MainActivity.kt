2299c913399d5d2c3c6ce89dbf1b7e2ff3bca84f
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-12 17:19:43 +0300

    Delete the data

--- app/src/main/java/com/bengisusahin/storingdata/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/storingdata/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var sharedPref : SharedPreferences
+    var ageFromPref : Int? = null //integer nullabell ve null atıyoruz
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +19,32 @@ class MainActivity : AppCompatActivity() {
 
         //SharedPreferences - XML - Key-Value kucuk veritabanlari
         sharedPref = getSharedPreferences("com.bengisusahin.storingdata", MODE_PRIVATE) //MODE_PRIVATE benim verielerime benim uygulamam ulassin sadece
+
+        ageFromPref = sharedPref.getInt("age", -1) //default value
+        if (ageFromPref == -1){ //demek ki kayitli bir veri yok
+            binding.textView.text = "Your age :"
+        }else{
+            binding.textView.text = "Your age : ${ageFromPref}"
+        }
     }
 
     fun save(view : View){
-        val myAge = binding.editText.text.toString()
-        binding.textView.text = "Your age: ${myAge}"
-        //binding.textView.text = "Your age: " + myAge
+        val myAge = binding.editText.text.toString().toIntOrNull()
+
+        if (myAge != null){
+            binding.textView.text = "Your age: ${myAge}"
+            //binding.textView.text = "Your age: " + myAge
+            sharedPref.edit().putInt("age",myAge).apply()
+        }
+
     }
 
     fun delete(view: View){
+        ageFromPref = sharedPref.getInt("age", -1)
 
+        if (ageFromPref != -1){
+            sharedPref.edit().remove("age").apply()
+            binding.textView.text = "Your age:"
+        }
     }
 }
